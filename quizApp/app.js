69d875189b34b4a269a5901ed365520949ed0e25
WardahIrshad <wardahirshad117@gmail.com>
2024-11-06 08:48:37 +0500

    Add files via upload

---
+++ quizApp/app.js
@@ -0,0 +1,105 @@
+let data = [
+    {
+        Q: "Q:JS is used for?",
+        a: "frontend",
+        b: "Animation",
+        c: "Dynamically update content",
+        d: "backend",
+        ans: "ans3",
+    },
+    {
+        Q: "Q:Full form of JS?",
+        a: "Javascript",
+        b: "javaScience",
+        c: "javaS",
+        d: "Javasecure",
+        ans: "ans1",
+    },
+    {
+        Q: "Q:Who developed JS?",
+        a: "santa",
+        b: "brenden Eich",
+        c: "george",
+        d: "Neck",
+        ans: "ans2",
+    },
+    {
+        Q: "Q:When JS developed?",
+        a: "1995",
+        b: "1990",
+        c: "1999",
+        d: "1892",
+        ans: "ans1",
+    },
+
+]
+
+let question = document.getElementById("qns");
+let option1 = document.getElementById("opt1");
+let option2 = document.getElementById("opt2");
+let option3 = document.getElementById("opt3");
+let option4 = document.getElementById("opt4");
+
+let answers = document.querySelectorAll(".options");
+let num = 0;
+let score = 0;
+let myScore = document.querySelector(".myScore");
+let nextBtn = document.getElementById("next");
+question.innerHTML = data[num].Q;
+option1.innerHTML = data[num].a;
+option2.innerHTML = data[num].b;
+option3.innerHTML = data[num].c;
+option4.innerHTML = data[num].d;
+
+function checkAns() {
+    let ans;
+    answers.forEach((curElement) => {
+        if (curElement.checked) {
+            ans = curElement.id;
+            console.log(ans);
+        }
+    })
+    return ans;
+}
+
+function disSelect() {
+    answers.forEach((curElement) => {
+        curElement.checked = false;
+    })
+}
+
+function next() {
+    let checkedAns = checkAns();
+    if (checkedAns == data[num].ans) {
+        score++;
+    }
+    num++;
+    disSelect();
+    if (num < data.length) {
+
+
+        question.innerHTML = data[num].Q;
+        option1.innerHTML = data[num].a;
+        option2.innerHTML = data[num].b;
+        option3.innerHTML = data[num].c;
+        option4.innerHTML = data[num].d;
+    } else {
+        myScore.innerHTML = `you scored ${score}/${data.length}<br/> <br/>
+ <button onclick="location.reload()">Play Again</button>`;
+        nextBtn.style.display = "none";
+        document.querySelector(".btn").style.backgroundColor = "lightBlue"
+
+    }
+
+}
+
+
+
+
+
+
+
+
+
+
+
